3061aa7a688cb17a988b7b4b89caa8ea654dcc46
yrong <yrong1997@gmail.com>
2020-05-25 21:09:06 +0800

    more example

---
+++ green-thread-1.rs
@@ -0,0 +1,66 @@
+#![feature(llvm_asm)]
+#![feature(naked_functions)]
+use std::io::Write;
+
+const SSIZE: isize = 1024;
+static mut S_PTR: *const u8 = 0 as *const u8;
+
+#[derive(Debug, Default)]
+#[repr(C)]
+struct ThreadContext {
+    rsp: u64,
+    r15: u64,
+    r14: u64,
+    r13: u64,
+    r12: u64,
+    rbx: u64,
+    rbp: u64,
+}
+
+fn print_stack(filename: &str) {
+    let mut f = std::fs::File::create(filename).unwrap();
+    unsafe {
+        for i in (0..SSIZE).rev() {
+            writeln!(
+                f,
+                "mem: {}, val: {}",
+                S_PTR.offset(i as isize) as usize,
+                *S_PTR.offset(i as isize)
+            )
+                .expect("Error writing to file.");
+        }
+    }
+}
+
+fn hello() {
+    println!("I LOVE WAKING UP ON A NEW STACK!");
+    print_stack("AFTER.txt");
+
+    loop {}
+}
+
+unsafe fn gt_switch(new: *const ThreadContext) {
+    llvm_asm!("
+        mov     0x00($0), %rsp
+        ret
+       "
+    :
+    : "r"(new)
+    :
+    : "alignstack"
+    );
+}
+
+fn main() {
+    let mut ctx = ThreadContext::default();
+    let mut stack = vec![0_u8; SSIZE as usize];
+    let stack_ptr = stack.as_mut_ptr();
+
+    unsafe {
+        S_PTR = stack_ptr;
+        std::ptr::write(stack_ptr.offset(SSIZE - 16) as *mut u64, hello as u64);
+        print_stack("BEFORE.txt");
+        ctx.rsp = stack_ptr.offset(SSIZE - 16) as u64;
+        gt_switch(&mut ctx);
+    }
+}
---
+++ tcp.rs
@@ -0,0 +1,103 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::mpsc;
+
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
+
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    thread::sleep(Duration::from_secs(1));
+    stream.flush().unwrap();
+}
